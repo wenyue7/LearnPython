1fe719961eaa13c98aeef1b14b6ba2af644b6b38
wenyue7 <872648180@qq.com>
2023-05-31 05:36:54 +0800

    update dic opt

--- 1.Base/1.syntax/2.baseData/using_dict.py
+++ 1.Base/1.syntax/2.baseData/using_dict.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python
 # Filename: using_dict.py
-# 'ab' is short for 'a'ddress'b'ook
-ab = { 'Swaroop' : 'swaroopch@byteofpython.info',
-'Larry' : 'larry@wall.org',
-'Matsumoto' : 'matz@ruby-lang.org',
-'Spammer' : 'spammer@hotmail.com'
-}
-print ("Swaroop's address is %s" % ab['Swaroop'])
+
+# 'm_dic' is short for 'a'ddress'b'ook
+m_dic = {'Swaroop' : 'swaroopch@byteofpython.info',
+        'Larry' : 'larry@wall.org',
+        'Matsumoto' : 'matz@ruby-lang.org',
+        'Spammer' : 'spammer@hotmail.com'}
+print("Swaroop's address is %s" % m_dic['Swaroop'])
 # Adding a key/value pair
-ab['Guido'] = 'guido@python.org'
+m_dic['Guido'] = 'guido@python.org'
+# 值可以取任何数据类型，但键必须是不可变的，如字符串，数字或元组
+m_dic[(10, 14)] = 'test@python.org'
 # Deleting a key/value pair
-del ab['Spammer']
-print ('\nThere are %d contacts in the address-book\n' % len(ab))
-for name, address in ab.items():
-  print ('Contact %s at %s' % (name, address))
-if 'Guido' in ab: # OR ab.has_key('Guido')
-  print ("\nGuido's address is %s" % ab['Guido'])
+del m_dic['Spammer']
+print('\nThere are %d contacts in the address-book\n' % len(m_dic))
+for name, address in m_dic.items():
+    print('Contact %s at %s' % (name, address))
+if 'Guido' in m_dic: # OR m_dic.has_key('Guido')
+    print("\nGuido's address is %s" % m_dic['Guido'])
+
+m_dic.clear()
+print('\nThere are %d contacts in the address-book\n' % len(m_dic))
