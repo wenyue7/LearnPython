e0124a58582f2e891bd61c7aa3cd1c7b4cdc3232
Hongjin Li <vic.hong@rock-chips.com>
2024-08-21 11:31:46 +0800

    update matplotlib base demo

--- 2.Tools/matplotlib/baseDemo.py
+++ 2.Tools/matplotlib/baseDemo.py
@@ -9,18 +9,18 @@ plt.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Matplotlib plot.
 # -- method 2
 
 fig_1 = plt.figure()  # an empty figure with no Axes
-fig_2, ax = plt.subplots()  # a figure with a single Axes
-fig_3, axs = plt.subplots(2, 2)  # a figure with a 2x2 grid of Axes
+fig_2, ax_2 = plt.subplots()  # a figure with a single Axes
+fig_3, axs_3 = plt.subplots(2, 2)  # a figure with a 2x2 grid of Axes
 
 ax_1 = fig_1.add_subplot(111)
 ax_1.plot([1, 2, 3, 4], [0, 2, 2, 2])  # Plot some data on the axes.
 
-ax.plot([1, 2, 3, 4], [2, 2, 2, 2])  # Plot some data on the axes.
+ax_2.plot([1, 2, 3, 4], [2, 2, 2, 2])  # Plot some data on the axes.
 
-axs[0, 0].plot([1, 2, 3, 4], [0, 2, 3, 4])  # Plot some data on the axes.
-axs[0, 1].plot([1, 2, 3, 4], [1, 2, 3, 4])  # Plot some data on the axes.
-axs[1, 0].plot([1, 2, 3, 4], [2, 2, 3, 4])  # Plot some data on the axes.
-axs[1, 1].plot([1, 2, 3, 4], [3, 2, 3, 4])  # Plot some data on the axes.
+axs_3[0, 0].plot([1, 2, 3, 4], [0, 2, 3, 4])  # Plot some data on the axes.
+axs_3[0, 1].plot([1, 2, 3, 4], [1, 2, 3, 4])  # Plot some data on the axes.
+axs_3[1, 0].plot([1, 2, 3, 4], [2, 2, 3, 4])  # Plot some data on the axes.
+axs_3[1, 1].plot([1, 2, 3, 4], [3, 2, 3, 4])  # Plot some data on the axes.
 
 # plt.show()
 
@@ -62,13 +62,13 @@ ax_1.plot(x, 1.6*x, marker="v", c="c", linestyle=":")  # Plot some data on the a
 
 # -- 附加信息
 
-ax.plot(x, x, label='linear')  # Plot some data on the axes.
-ax.plot(x, x**2, label='quadratic')  # Plot more data on the axes...
-ax.plot(x, x**3, label='cubic')  # ... and some more.
-ax.set_xlabel('x label')  # Add an x-label to the axes.
-ax.set_ylabel('y label')  # Add a y-label to the axes.
-ax.set_title("Simple Plot")  # Add a title to the axes.
-ax.legend()  # Add a legend.
+ax_2.plot(x, x, label='linear')  # Plot some data on the axes.
+ax_2.plot(x, x**2, label='quadratic')  # Plot more data on the axes...
+ax_2.plot(x, x**3, label='cubic')  # ... and some more.
+ax_2.set_xlabel('x label')  # Add an x-label to the axes.
+ax_2.set_ylabel('y label')  # Add a y-label to the axes.
+ax_2.set_title("Simple Plot")  # Add a title to the axes.
+ax_2.legend()  # Add a legend.
 
 # ========================================================================
 
